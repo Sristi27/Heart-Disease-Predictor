b6cbc2596cc5efec16047db92628394235cfdc66
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-06-17 13:46:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pickle
+from flask import Flask,request,jsonify,render_template
+from flask_cors import CORS
+
+app=Flask(__name__)
+heart_model = pickle.load(open('heart_model.pkl','rb'))
+CORS(app)
+
+@app.route('/',methods=['POST'])
+def predict():
+    
+    features=[]
+    features.append(float(request.form['age']))
+    features.append(float(request.form['sex']))
+    features.append(float(request.form['cp']))
+    features.append(float(request.form['trestbps']))
+    features.append(float(request.form['chol']))
+    features.append(float(request.form['fbs']))
+    features.append(float(request.form['restecg']))
+    features.append(float(request.form['thalach']))
+    features.append(float(request.form['exang']))
+    features.append(float(request.form['oldpeak']))
+    features.append(float(request.form['slope']))
+    features.append(float(request.form['ca']))
+    features.append(float(request.form['thal']))
+    #l=[63,1,3,145,233,1,0,150,0,2.3,0,0,1]
+    #l=[float(x) for x in l]
+    output=heart_model.predict([np.array(features)])
+    if(output[0]==1): value= "Yes"
+    else: value= "No"
+    return jsonify(msg="Succesfully fetched",value=value)
+    
+
+#@app.route('/',methods=['GET'])
+#def home():
+#    return jsonify(msg="Detection API working perfectly!")
+    
+if __name__=='main':
+    app.run(debug=True)
+
+
