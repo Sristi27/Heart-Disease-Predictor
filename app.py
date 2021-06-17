7f3c75645a256b881e4e3b510eb5a902238b4f60
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-06-17 22:06:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 heart_model = pickle.load(open('heart_model.pkl','rb'))
 CORS(app)
 
-@app.route('/',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     
     features=[]
